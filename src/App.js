590892f201b40565d9ba7efa3ea19322f2515544
Marcos <hoggm@udesa.edu.ar>
2021-04-29 20:01:53 -0300

    cambios api

--- src/App.js
+++ src/App.js
@@ -1,32 +1,74 @@
 
 import './App.css';
-import Header from './components/Header' ;
-import Footer from './components/Footer';
-import Perfil from './components/Perfil';
+import Header from './Components/Header' ;
+import Footer from './Components/Footer';
+// import Perfil from './components/Perfil';
+import React, {Component} from "react";
 
+class App extends Component {
 
+  constructor (props){
+    super(props);
+    this.state={
+      listaPersonas: [],
 
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-
-        <p>
-         Users 
-        </p>
+    }
+  }
+componentDidMount(){
+fetch ("https://randomuser.me/api/?results=20")
+  .then (r=> r.json())
+  .then ((resultado) => {
+    console.log(resultado)
+    this.setState({listaPersonas: resultado})
     
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </header>
+  })
 
-    </div>
-  );
+
+
 }
 
+render(){
+console.log(this.state.listaPersonas.results[])
+ return (//<></>
+  <p>{this.state.listaPersonas[0].name.first}</p>
+    // <div className="App">
+    //   <header className="App-header">
+
+    //     <p>
+    //      Users 
+    //     </p>
+    
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //     </a>
+    //   </header>
+
+    // </div>
+  
+//Foto del contacto.
+// Apellido
+// Nombre del contacto.
+// Email.
+// Fecha de nacimiento y edad entre paréntesis.
+
+//   <div className="App">
+//        <Header/>
+//        <div className="tarjetas">
+   
+//     </div>
+//         <Footer/>
+//     </div>
+   );
+  
+}// llave de la clase
+
+
+
+
+
+}
 export default App;
